fc8c147f9e461cb04caa926b18f48a8fec05810e
jacobaldo <aldochd13@gmail.com>
2025-09-17 00:52:08 -0500

    add tailwind

---
+++ babel.config.js
@@ -0,0 +1,36 @@
+module.exports = function (api) {
+  api.cache(true);
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@": "./src",
+            "@env": "./src/lib/env.js",
+            "@images": "./assets/images",
+          },
+          extensions: [
+            ".ios.ts",
+            ".android.ts",
+            ".ts",
+            ".ios.tsx",
+            ".android.tsx",
+            ".tsx",
+            ".jsx",
+            ".js",
+            ".json",
+          ],
+        },
+      ],
+      "react-native-reanimated/plugin",
+      "react-native-worklets/plugin",
+    ],
+  };
+};
