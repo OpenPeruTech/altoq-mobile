bb2e8562c78acb15db1184e49ea1ecc3957d5b49
TadeoPG <tadeomanuelportillo@gmail.com>
2025-09-28 06:04:53 -0500

    fix: fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@ const colors = require("./src/components/ui/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   presets: [require("nativewind/preset")],
   darkMode: "class",
   theme: {
