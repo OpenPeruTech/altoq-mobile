79b4d48a0a0463511fe47cf298397139997db737
TadeoPG <tadeomanuelportillo@gmail.com>
2025-09-28 06:49:57 -0500

    fix: fix babel config

--- babel.config.js
+++ babel.config.js
@@ -1,32 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: [
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
-      [
-        "module-resolver",
-        {
-          root: ["./"],
-          alias: {
-            "@": "./src",
-            "@env": "./src/lib/env.js",
-            "@images": "./assets/images",
-          },
-          extensions: [
-            ".ios.ts",
-            ".android.ts",
-            ".ts",
-            ".ios.tsx",
-            ".android.tsx",
-            ".tsx",
-            ".jsx",
-            ".js",
-            ".json",
-          ],
-        },
-      ],
-      "react-native-reanimated/plugin",
     ],
+    plugins: ["react-native-reanimated/plugin"],
   };
 };
